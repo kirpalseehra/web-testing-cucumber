e80784181c7d3b4fc05116bbc67e2dd1a4d23ffa
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-02-08 09:08:09 +0000

    fixed the password too long scenario error that kept occurring

--- cucumber_bbc_login/features/step_definitions/bbc_login_step_defs.rb
+++ cucumber_bbc_login/features/step_definitions/bbc_login_step_defs.rb
@@ -31,19 +31,19 @@ Then("I receieve an error for not having a long enough password") do
   expect(@bbc_site.bbc_loginpage.password_error_message).to eq @bbc_site.bbc_loginpage.short_password
 end
 
-# Inputting correct username credentials but incorrect corresponding password details for that account
-Given("I input correct username details") do
-  @bbc_site.bbc_loginpage.fill_in_username('kirpal-seehra@hotmail.co.uk')
-end
+# # Inputting correct username credentials but incorrect corresponding password details for that account
+# Given("I input correct username details") do
+#   @bbc_site.bbc_loginpage.fill_in_username('kirpal-seehra@hotmail.co.uk')
+# end
 
-Given("I input the incorrect password details") do
-  @bbc_site.bbc_loginpage.fill_in_password('p31423xxasxa!')
-end
+# Given("I input the incorrect password details") do
+#   @bbc_site.bbc_loginpage.fill_in_password('p31423xxasxa!')
+# end
 
-Then("I receieve an error for not having the right password for that account") do
-  expect(@bbc_site.bbc_loginpage.password_error_message).to eq(@bbc_site.bbc_loginpage.incorrect_password) | eq(@bbc_site.bbc_loginpage.reset_password)
-  sleep 10
-end
+# Then("I receieve an error for not having the right password for that account") do
+#   expect(@bbc_site.bbc_loginpage.password_error_message).to eq(@bbc_site.bbc_loginpage.incorrect_password) | eq(@bbc_site.bbc_loginpage.reset_password)
+#   sleep 10
+# end
 
 # Inputting a username that is too short in character length
 Given("I input a username thats too short") do
@@ -56,9 +56,10 @@ end
 
 # Inputting a password that is too long in character length
 Given("I input a password with too many characters") do
-  @bbc_site.bbc_loginpage.fill_in_password('jasdijsaiodjsaoidjaisodjoiasjdoiasjodjsaiojdoiasjdoij321')
+  @bbc_site.bbc_loginpage.fill_in_password('sadsadsadsadsadaadsdsadsadsadasasadsadasdadadasdadaasasdasdasasdaadsadasadasdjasdijsaiodjsaoidjaisodjoiasjdoiasjodjsasasaaiojdoiasjdoij321')
 end
 
 Then("I receieve an error for having a password too long") do
-  expect(@bbc_site.bbc_loginpage.password_error_messege).to eq @bbc_site.bbc_loginpage.long_password
+  sleep 10
+  expect(@bbc_site.bbc_loginpage.password_error_message).to eq @bbc_site.bbc_loginpage.long_password
 end
